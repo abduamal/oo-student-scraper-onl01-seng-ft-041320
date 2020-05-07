ec75497a82182fb5d80d1536bde123f7dd13dfb2
Amal Abdu <abduamal@ymail.com>
2020-05-07 19:30:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    students = []
+    index = Nokogiri::HTML(html)
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
