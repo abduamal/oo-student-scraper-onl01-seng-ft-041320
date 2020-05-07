91bad5cabc34a3c76e262558017e49cd2e826d53
Amal Abdu <abduamal@ymail.com>
2020-05-07 20:01:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student_hash|
-      self.new(student_hash)
+    students_array.each do |student|
+      self.new(student)
     end
   end
 
