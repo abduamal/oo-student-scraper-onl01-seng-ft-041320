d426dcfa671f8a2b160e964ed5b9af59ef977b65
Amal Abdu <abduamal@ymail.com>
2020-05-07 19:48:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,12 +7,8 @@ class Scraper
     html = open(index_url)
     students = []
     index = Nokogiri::HTML(html)
-    index.css("div.student-card").each do |student|
-      student_details = {}
-      student_details[:name] = student.css("h4.student-name").text
-      student_details[:location] = student.css("p.student-location").text
-      profile_path = student.css("a").attribute("href").value
-      student_details[:profile_url] = './fixtures/student-site/' + profile_path
+    index.css(".student-card").create_from_collection
+    ofile_url] = './fixtures/student-site/' + profile_path
       students << student_details
     end
     students
